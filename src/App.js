c7c8c5850ce4a34a059737687f81374cdec605fa
Miiapetri <101567276+Miiapetri@users.noreply.github.com>
2022-10-03 19:46:58 +0300

    Tavoitesyke

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [heartRate, setheartRate] = useState();
+  const [Age, setAge] = useState();
+  const [upperRate, setupperRate] = useState();
+  const [lowerRate, setlowerRate] = useState();
+
+  function calculate() {
+    setupperRate((220-Age)*0.85);
+    setlowerRate((220-Age)*0.65);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Heart rate limits calculator</h3>
+      <form>
+        <div>
+          <label>Age </label>
+          <input type="number" value= {Age} onChange={e=> setAge(e.target.value)} />
+        </div>
+        <div>
+          <label>Heart rate limits </label>
+          <output>{lowerRate}-{upperRate}</output>
+        </div>
+        <button type="button" onClick={calculate}>Calculate</button>
+      </form>
     </div>
   );
 }
